48f8de9708dd841ace0b481eaecf99c855dfc4bd
Shubham Shahare <shubhamshahare6@gmail.com>
2016-09-09 00:18:52 +0530

    register_table created

--- regs.c
+++ regs.c
@@ -5,4 +5,5 @@ struct reg_set{
   enum type reg_type;
 };
 typedef struct reg_set r;
-r reg_set[20]"al reg8","ah reg8","ax reg16","eax reg32","bh reg8","bl reg8","bx reg16","ebx reg32","cl reg8","ch reg8","cx reg16","ecx reg32","dl reg8","dh reg8","dx reg16","edx reg32","esi reg32","edi reg32","esp reg32","ebp reg32"};
+r reg_set[20]={{"al",reg8},{"ah",reg8},{"ax",reg16},{"eax",reg32},{"bh",reg8},{"bl",reg8},{"bx",reg16},{"ebx",reg32},{"cl",reg8},{"ch",reg8},{"cx",reg16},{"ecx",reg32},{"dl",reg8},{"dh",reg8},{"dx",reg16},{"edx",reg32},{"esi",reg32},{"edi",reg32},{"esp",reg32},{"ebp",reg32}};
+main(){}
--- regs.py
+++ regs.py
@@ -1,5 +1,3 @@
-from sys import argv
-#filename = "regs.c"
 file = open('regs.dat','r')
 f =  file.readlines()
 ou = open('regs.c','w')
@@ -10,11 +8,13 @@ ou.write("  char register_name[6];\n")
 ou.write("  enum type reg_type;\n")
 ou.write("};\n")
 ou.write("typedef struct reg_set r;\n")
+a = map(lambda x:x.rstrip(),f)
+d = map(lambda x:x.split(),a)
+l=d
 
-ou.write("r reg_set[%d]" %len(f))
-for line in range(0,len(f)-1):
-    ou.write("\""+(f[line]).rstrip()+"\"")
-    ou.write(",")
-ou.write("\""+f[len(f)-1].rstrip()+"\"")
-ou.write("};");
+ou.write("r reg_set[%d]={" %len(f))
+for d in range(0,len(f)-1):
+  ou.write("{\""+l[d][0]+"\","+l[d][1]+"},")
+ou.write("{\""+l[len(f)-1][0]+"\","+l[len(f)-1][1]+"}};\n")
 
+ou.write("main(){}");
